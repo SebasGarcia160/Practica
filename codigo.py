62c3d8b7cfd9bb0953a830a0499218a2ba84eb82
SebasGarcia160 <joan.garcia@salesiana.edu.co>
2024-11-04 17:18:56 -0500

    Primer Repositorio

---
+++ codigo.py
@@ -0,0 +1,14 @@
+# Acertijo de lógica
+print("Acertijo: Estoy en el principio de la eternidad, al final del tiempo y el espacio, en el principio de cada final y en el final de cada lugar. ¿Qué soy?")
+
+# Respuesta correcta
+respuesta_correcta = "la letra e"
+
+# Captura la respuesta del usuario
+respuesta_usuario = input("Escribe tu respuesta: ").strip().lower()
+
+# Comprobación de la respuesta
+if respuesta_usuario == respuesta_correcta:
+    print("¡Correcto! Has resuelto el acertijo.")
+else:
+    print("Incorrecto. La respuesta correcta era: 'La letra E'.")
